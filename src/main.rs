19cee2ed884b165872b9c45d7fed67b36359f6c5
Максим Тимощук <git@github.com:tabinchik/test.git>
2024-09-29 12:49:07 +0300

    some match tasks

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod Basic_Types;
 mod Ownership;
 mod Compound_Types;
 mod flow_control;
+mod pattern_match;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/pattern_match/match/mod.rs
@@ -0,0 +1,4 @@
+mod match_1;
+mod match_2;
+mod match_3;
+mod match_4;
