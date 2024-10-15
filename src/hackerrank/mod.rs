6cc09726a2321e8380bb62bc833cc60d66eab819
Максим Тимощук <git@github.com:tabinchik/test.git>
2024-10-15 12:08:45 +0300

    hackerrank tasks

--- src/hackerrank/mod.rs
+++ src/hackerrank/mod.rs
@@ -5,3 +5,6 @@ mod compare_the_triplets;
 mod a_very_big_sum;
 mod diagonal_difference;
 mod plus_minus;
+mod staircase;
+mod mini_max_sum;
+mod birthday_cake;
---
+++ src/hackerrank/staircase.rs
@@ -0,0 +1,31 @@
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fn staircase(n: i32) {
+    for i in 1..=n {
+        let spaces = n - i;
+        for _ in 0..spaces {
+            print!(" ");
+        }
+
+        for _ in 0..i {
+            print!("#");
+        }
+
+        println!();
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    staircase(n);
+}
