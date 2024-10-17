2b119e4890218d36f61b2cd6db1402e7a4b74160
Максим Тимощук <git@github.com:tabinchik/test.git>
2024-10-17 09:45:21 +0300

    lifetime/static

---
+++ src/lifetime/static/task1.rs
@@ -0,0 +1,12 @@
+#[test]
+/* Fill in the blank in two ways */
+fn main() {
+    let v: &str = "hello";
+    need_static(v);
+
+    println!("Success!")
+}
+
+fn need_static(r : &'static str) {
+    assert_eq!(r, "hello");
+}
