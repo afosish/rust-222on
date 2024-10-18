475813726e65a557a67bd7c4aea79fdf7422b9f8
Максим Тимощук <git@github.com:tabinchik/test.git>
2024-10-18 17:52:48 +0300

    practice10

---
+++ src/practice/task8.rs
@@ -0,0 +1,34 @@
+fn rotate(s: String, n: isize) -> String {
+    let len = s.len();
+    let n = ((n % len as isize) + len as isize) % len as isize;
+
+    let split_index = len - n as usize;
+    let (left, right) = s.split_at(split_index);
+
+    format!("{}{}", right, left)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let s = "abcdefgh".to_string();
+        let shifts = [
+            (0, "abcdefgh"),
+            (8, "abcdefgh"),
+            (-8, "abcdefgh"),
+            (1, "habcdefg"),
+            (2, "ghabcdef"),
+            (10, "ghabcdef"),
+            (-1, "bcdefgha"),
+            (-2, "cdefghab"),
+            (-10, "cdefghab"),
+        ];
+
+        shifts.iter().for_each(|(n, exp)| {
+            assert_eq!(rotate(s.clone(), *n), exp.to_string());
+        });
+    }
+}
